4625be31d5d00705d20954c83476bded6c3c219e
Wes Moser <Moser.Wesley@gmail.com>
2014-02-15 11:14:29 -0500

    Initial Commit

---
+++ Practice/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Practice
+//
+//  Created by Moser, Wesley on 2/15/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
