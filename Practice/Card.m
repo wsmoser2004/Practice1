481b42a899e5423ebe7f6596d19d84944a4995d1
Wes Moser <Moser.Wesley@gmail.com>
2014-02-15 11:58:09 -0500

    Do Practice.

---
+++ Practice/Card.m
@@ -0,0 +1,27 @@
+//
+//  Card.m
+//  Practice
+//
+//  Created by Moser, Wesley on 2/15/14.
+//  Copyright (c) 2014 Moser, Wesley. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+- (int)match:(Card *)aCard
+{
+    if ([aCard.contents isEqualToString:self.contents] &&
+        aCard.matched == self.matched &&
+        aCard.chosen == self.chosen)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+@end
